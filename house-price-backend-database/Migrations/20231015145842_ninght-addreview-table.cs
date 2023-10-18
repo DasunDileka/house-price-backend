4b30dd9a22ebb68eca4b3dddc9eb962030032a72
Dasun Perera <dasun.perera@acentura.com>
2023-10-18 10:43:42 +0530

    add review table

---
+++ house-price-backend-database/Migrations/20231015145842_ninght-addreview-table.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace house_price_backend_database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ninghtaddreviewtable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Images",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Review",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    comment = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Review", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Review_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_UserId",
+                table: "Images",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Review_UserId",
+                table: "Review",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Users_UserId",
+                table: "Images",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Users_UserId",
+                table: "Images");
+
+            migrationBuilder.DropTable(
+                name: "Review");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Images_UserId",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Images");
+        }
+    }
+}
---
+++ house-price-backend-database/Model/Review.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace house_price_backend_database.Model
+{
+    public partial class Review
+    {
+        public virtual User User { get; set; }
+        public int Id { get; set; }
+        public string comment { get; set; }
+    }
+}
